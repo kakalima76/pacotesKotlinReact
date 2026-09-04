1f35c2d761552c61eab44a868b6d43e8f92808a2
kakalima76 <nieraldo@gmail.com>
2026-09-04 00:03:38 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,27 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { useEffect , useState} from 'react';
+import {
+  NativeModules,
+  StatusBar,
+  StyleSheet,
+  useColorScheme,
+  View,
+  Text,
+  
+} from 'react-native';
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
 
+
+
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
+  
+
+  
   return (
     <SafeAreaProvider>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
@@ -24,14 +31,24 @@ function App() {
 }
 
 function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
+  const [resultado, setResultado] = useState<number | null>(null);
+
+
+  useEffect(() => {
+  NativeModules.PersonsFunctions.somar(100, 20)
+    .then((valor: number) => {
+      setResultado(valor);
+    })
+    .catch((erro: unknown) => {
+      console.error('Erro ao chamar Kotlin:', erro);
+    });
+}, []);
 
   return (
     <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
+      <Text style={styles.text}>
+        Resultado : {resultado}
+      </Text>
     </View>
   );
 }
@@ -39,6 +56,9 @@ function AppContent() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  text: {
+    fontSize: 30,
   },
 });
 
