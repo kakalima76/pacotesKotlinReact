b07511cfca5ef8d78def0fc0c6708c3df1adc3b2
kakalima76 <nieraldo@gmail.com>
2026-09-08 13:52:18 -0300

    mapbox marker

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,13 +1,39 @@
 import Mapbox from '@rnmapbox/maps';
-import { View } from 'react-native';
+import { Image, View } from 'react-native';
 import { MAPBOX_ACCESS_TOKEN } from '../../config/mapbox';
+import { useMainContext } from '../../contexts';
 
 Mapbox.setAccessToken(MAPBOX_ACCESS_TOKEN);
 
 export function HomeScreen() {
+  const { location } = useMainContext();
+
+  if (!location) {
+    return <View className="flex-1" />;
+  }
+
   return (
     <View className="flex-1">
-      <Mapbox.MapView style={{ flex: 1 }} />
+      <Mapbox.MapView style={{ flex: 1 }}>
+        <Mapbox.Camera
+          centerCoordinate={[location.longitude, location.latitude]}
+          zoomLevel={15}
+        />
+
+        <Mapbox.MarkerView
+          id="vehicle"
+          coordinate={[location.longitude, location.latitude]}
+        >
+          <Image
+            source={require('../../assets/car.png')}
+            style={{
+              width: 72,
+              height: 72,
+            }}
+            resizeMode="contain"
+          />
+        </Mapbox.MarkerView>
+      </Mapbox.MapView>
     </View>
   );
 }
